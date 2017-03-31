e9a6ee1b87c4e24374bd13b34bacc3f0df85a22d
Unknown <wcarrasco@mail.csuchico.edu>
2017-03-30 21:21:00 -0700

    commit

--- sales.js
+++ sales.js
@@ -3,22 +3,26 @@ var runningTotal = 0.0;
 function addItem()
 {
   var newItem;
-  newItem = document.getElementById("price").number();
-  if (newItem != "isNaN") {
-    alert("Enter price as a number");  
+  newItem = document.getElementById("price").value;
+  if(isNaN(newItem) == true) {
+    alert("Enter price as a number")
 }
-
-
-  //IF newItem is not a number
-  // THEN show an alert: "Enter price as a number"
-  //OTHERWISE,
-  // update newItem to its value cast as a number
-  // update runningTotal to be its value plus newItem
-  // create a variable called dollars
-  // call asCurrency() by with the value of runningTotal and assign the return value to dollars
-  // update the innerHTML of the span with the id "subtotal" to be dollars
-  // update the value of the input with the id "price" to be an empty string
-  // update a cookie called "preTax" with the value of runningTotal
+ else{
+  newItem = number(newItem)
+  var runningTotal += newItem;
+  var dollars;
+  dollars = asCurrency(runningTotal);
+}
+//IF newItem is not a number
+ // THEN show an alert: "Enter price as a number"
+ //OTHERWISE,
+ // update newItem to its value cast as a number
+ // update runningTotal to be its value plus newItem
+ // create a variable called dollars
+ // call asCurrency() by with the value of runningTotal and assign the return value to dollars
+ // update the innerHTML of the span with the id "subtotal" to be dollars
+ // update the value of the input with the id "price" to be an empty string
+ // update a cookie called "preTax" with the value of runningTotal
 }
 
 //takes a number and gives a string with the number displayed as USD currency
@@ -28,7 +32,7 @@ function asCurrency(val)
 }
 
 //courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
-function setCookie(cname, cvalue, exdays) {
+function setCookie(cvalue) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     var expires = "expires="+d.toUTCString();
